04351cfaf544e709bd546151cc0e99417a1f181b
丁汉飞 <dinghf@greatId.com>
2017-03-31 21:53:47 +0800

    change git address

---
+++ components/myProfileView/index.js
@@ -0,0 +1,319 @@
+'use strict';
+
+var apiKey = "o6yuauaw7f5m56jb";
+var el = new Everlive(apiKey);
+var username;
+var address;
+var phone;
+var userid;
+var state;
+var avatar;
+var status;
+var email;
+var card;
+var introduceName;
+var introduceContact;
+var introduceEmail;
+
+var currentPoint;
+var lastqwarded;
+
+app.myProfileView = kendo.observable({
+    onShow: function () {
+        $("#rightview").text("Edit");
+
+        if (app.currentUser.Id != "") {
+            userid = app.currentUser.Id;
+            username = app.currentUser.Username;
+            email = app.currentUser.Email;
+            address = app.currentUser.DeliveryAddress;
+            phone = app.currentUser.ContactNumber;
+            state = app.currentUser.State;
+            status = app.currentUser.Status;
+            card = app.currentUser.Card;
+            avatar = app.currentUser.Avatar;
+            introduceName = app.currentUser.IntroducerName;
+            introduceContact = app.currentUser.IntroducerContact;
+            introduceEmail = app.currentUser.IntroducerEmail;
+
+            currentPoint = app.currentUser.CurrentPoint;
+            lastqwarded = app.currentUser.LatestAwardedPoint;
+
+            app.myProfileView.set("usernameinfo", username);
+            app.myProfileView.set("emailinfo", email);
+            app.myProfileView.set("defaultaddress", address);
+            app.myProfileView.set("phone", phone);
+            app.myProfileView.set("state", state);
+            app.myProfileView.set("status", status);
+            app.myProfileView.set("card", card);
+            $('#layout_point').html("PV "+currentPoint);
+
+            $("#save").attr("disabled", true);
+            $("#usernameinfo").attr("disabled", true);
+            $("#emailinfo").attr("disabled", true);
+            $("#defaultaddress").attr("disabled", true);
+            $("#phone").attr("disabled", true);
+            $("#state").attr("disabled", true);
+            $("#status").attr("disabled", true);
+            $("#card").attr("disabled", true);
+            $("#introduce_name").attr("disabled", true);
+            $("#introduce_contact").attr("disabled", true);
+            $("#introduce_email").attr("disabled", true);
+
+            if (introduceName != "") {
+                app.myProfileView.set("introduce_name", introduceName);
+            }
+
+            if (introduceContact != "") {
+                app.myProfileView.set("introduce_contact", introduceContact);
+            }
+
+            if (introduceEmail != "") {
+                app.myProfileView.set("introduce_email", introduceEmail);
+            }
+
+
+            var imagehead = document.getElementById("userheadview");
+            if (avatar != null) {
+                el.Files.getById(avatar).then(function (data) {
+                        for (var item in data) {
+                            if (item == 'result') {
+                                var datainside = data[item];
+                                for (var iteminside in datainside) {
+                                    if (iteminside == 'Uri') {
+                                        var iamgeurl = datainside[iteminside];
+                                        imagehead.src = iamgeurl;
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    function (error) {
+                        alert(JSON.stringify(error));
+                    });
+            } else {
+                imagehead.src = "/resources/head.png";
+            }
+        } else {
+            $('#layout_point').html("PV4445555");
+           /* navigator.notification.alert("You do not login,Please login first.");
+            setTimeout(function () {
+                app.mobileApp.navigate('components/loginModelView/view.html');
+            }, 10);*/
+        }
+
+        /*el.Users.currentUser().then(function (data) {
+         for (var item in data) {
+         if (item == 'result') {
+         if (data[item] == null) {
+         alert("You do not login,Please login first.");
+         app.mobileApp.navigate('components/loginModelView/view.html');
+         } else {
+         var datainside = data[item];
+         for (var iteminside in datainside) {
+         if (iteminside == 'Username') {
+         username = datainside[iteminside];
+         } else if (iteminside == 'DeliveryAddress') {
+         address = datainside[iteminside]
+         } else if (iteminside == 'ContactNumber') {
+         phone = datainside[iteminside]
+         } else if (iteminside == 'Id') {
+         userid = datainside[iteminside]
+         } else if (iteminside == 'State') {
+         state = datainside[iteminside]
+         } else if (iteminside == 'Avatar') {
+         avatar = datainside[iteminside]
+         } else if (iteminside == 'Email') {
+         email = datainside[iteminside]
+         } else if (iteminside == 'Card') {
+         card = datainside[iteminside]
+         } else if (iteminside == 'Status') {
+         status = datainside[iteminside]
+         }
+
+         }
+         }
+         }
+         }
+         },
+         function (error) {
+         alert(JSON.stringify(error));
+         });*/
+
+    },
+    afterShow: function () {
+
+    },
+    editprofile: function () {
+        var righttitle = $("#rightview").text();
+
+        if (righttitle == "Edit") {
+            $("#rightview").text("Save");
+            $("#save").attr("disabled", false);
+            $("#usernameinfo").attr("disabled", false);
+            $("#defaultaddress").attr("disabled", false);
+            $("#phone").attr("disabled", false);
+            $("#state").attr("disabled", false);
+            $("#emailinfo").attr("disabled", false);
+            $("#status").attr("disabled", false);
+            $("#card").attr("disabled", false);
+            $("#introduce_name").attr("disabled", false);
+            $("#introduce_contact").attr("disabled", false);
+            $("#introduce_email").attr("disabled", false);
+
+        } else if (righttitle == "Save") {
+            var username = $("#usernameinfo").val();
+            var address = $("#defaultaddress").val();
+            var phone = $("#phone").val();
+            var card = $("#card").val();
+            var state = $("#state").val();
+            var status = $("#status").val();
+            var introduceName = $("#introduce_name").val();
+            var introduceContact = $("#introduce_contact").val();
+            var introduceEmail = $("#introduce_email").val();
+            var email = $("#emailinfo").val();
+
+            if (username == "") {
+                alert("username can not empty");
+            } else {
+                if (phone == "") {
+                    alert("phone can not empty");
+                } else {
+                    if (address == "") {
+                        alert("address can not empty");
+                    } else {
+                        var re = /^1\d{10}$/;
+                        if (re.test(phone)) {
+                            alert("Update "+username+" "+card+" "+phone+" "+state);
+
+                            el.Users.updateSingle({
+                                    'Id': userid,
+                                    'Username': username,
+                                    'DeliveryAddress': address,
+                                    'ContactNumber': phone,
+                                    'Card': card,
+                                    'State': state,
+                                    'Status': status,
+                                    'IntroducerName': introduceName,
+                                    'IntroducerContact': introduceContact,
+                                    'IntroducerEmail': introduceEmail,
+                                    'Email': email
+                                },
+                                function (data) {
+                                    alert("Update myProfile Info successfully");
+                                    $("#rightview").text("Edit");
+                                    $("#save").attr("disabled", true);
+                                    $("#usernameinfo").attr("disabled", true);
+                                    $("#defaultaddress").attr("disabled", true);
+                                    $("#phone").attr("disabled", true);
+                                    $("#state").attr("disabled", true);
+                                    $("#emailinfo").attr("disabled", true);
+                                    $("#status").attr("disabled", true);
+                                    $("#card").attr("disabled", true);
+                                    $("#introduce_name").attr("disabled", true);
+                                    $("#introduce_contact").attr("disabled", true);
+                                    $("#introduce_email").attr("disabled", true);
+
+                                },
+                                function (error) {
+                                    alert(JSON.stringify(error));
+                                });
+                        } else {
+                            alert("The phone is not valid");
+                        }
+                    }
+                }
+            }
+        }
+
+
+    },
+
+
+    /*addressdetail: function () {
+     alert("address detail");
+     },*/
+
+    /* saveprofile: function () {
+     var username = $("#usernameinfo").val();
+     var address = $("#defaultaddress").val();
+     var phone = $("#phone").val();
+
+     if(username==""){
+     alert("username can not empty");
+     }else {
+     if (phone==""){
+     alert("phone can not empty");
+     }else {
+     if (address==""){
+     alert("address can not empty");
+     }else {
+     var re = /^1\d{10}$/
+     if (re.test(phone)) {
+     //alert("usernae---  "+username+"--address-- "+address+"--phone-- "+phone);
+     el.Users.updateSingle({ 'Id': userid, 'Username': username, 'DeliveryAddress': address, 'ContactNumber': phone },
+     function(data){
+     alert(JSON.stringify(data));
+     },
+     function(error){
+     alert(JSON.stringify(error));
+     });
+     } else {
+     alert("The phone is not valid");
+     }
+     }
+     }
+     }
+     },*/
+});
+app.localization.registerView('myProfileView');
+
+app.myloyaltypointView = kendo.observable({
+    onShow: function () {
+        /*currentPoint = "PV 24000";
+        lastqwarded = "PV 1400";*/
+        $("#current_point").attr("disabled", true);
+        $("#last_awarded").attr("disabled", true);
+        app.myloyaltypointView.set("current_point", "PV "+currentPoint);
+        app.myloyaltypointView.set("last_awarded", "PV "+lastqwarded);
+
+        /* JsBarcode("#barcode4",
+         "f701bc80-db33-11e6-ba7d-ed8ffe6e33d3",
+         {displayValue: true});*/
+        JsBarcode("#barcode4", userid, {displayValue: true});
+
+        var img = document.getElementById('barcode4');
+        img.style.width = "100%"
+        img.style.height = "auto"
+
+       /* var qrcode = new QRCode(document.getElementById("qrcode"), {
+            width : 200,
+            height : 200
+        });
+
+        qrcode.makeCode(userid);*/
+
+    },
+    buyproduct: function () {
+        alert("The buy product.");
+        app.mobileApp.navigate('components/productListView/view.html?filter=' + encodeURIComponent(JSON.stringify({
+                field: 'pvPrice',
+                value: dataItem.Id,
+                operator: 'eq'
+            })));
+
+    },
+});
+
+
+app.localization.registerView('myloyaltypointView');
+
+// START_CUSTOM_CODE_myProfileView
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_myProfileView
+
+// START_CUSTOM_CODE_myProfileViewModel
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_myProfileViewModel
---
+++ components/productListView/index.js
@@ -0,0 +1,325 @@
+'use strict';
+var keyword;
+var filter;
+var curItem;
+var photoDS;
+app.productListView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+app.localization.registerView('productListView');
+
+// START_CUSTOM_CODE_productListView
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_productListView
+(function(parent) {
+    var dataProvider = app.data.backendServices,
+        /// start global model properties
+        /// end global model properties
+
+        fetchFilteredData = function(paramFilter, searchFilter) {
+            var model = parent.get('productListViewModel'),
+                dataSource;
+
+            if (model) {
+                dataSource = model.get('dataSource');
+            } else {
+                parent.set('productListViewModel_delayedFetch', paramFilter || null);
+                return;
+            }
+
+            if (paramFilter) {
+                model.set('paramFilter', paramFilter);
+            } else {
+                model.set('paramFilter', undefined);
+            }
+
+            if (paramFilter && searchFilter) {
+                dataSource.filter({
+                    logic: 'and',
+                    filters: [paramFilter, searchFilter]
+                });
+            } else if (paramFilter || searchFilter) {
+                dataSource.filter(paramFilter || searchFilter);
+            } else {
+                dataSource.filter({});
+            }
+        },
+
+        flattenLocationProperties = function(dataItem) {
+            var propName, propValue,
+                isLocation = function(value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
+                }
+            }
+        },
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'Product',
+                dataProvider: dataProvider
+            },
+            change: function(e) {
+                var data = this.data();
+                filter = $("#filterSelected").val();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+                    var name = dataItem['ProductName'];
+                    if (keyword!=null && name.toLowerCase().indexOf(keyword.toLowerCase())==-1){
+                        this.data().remove(dataItem);
+                        continue;
+                    }else if (filter!=null){
+                        if (filter == '10' && parseInt(dataItem["pvPrice"])>0) {
+                            this.data().remove(dataItem);
+                            continue;
+                        }else if (filter == '01' && parseInt(dataItem["cvPrice"])>0) {
+                            this.data().remove(dataItem);
+                            continue;
+                        }else if (filter == '11' && parseInt(dataItem["cvPrice"])==0 && parseInt(dataItem["pvPrice"])==0) {
+                            this.data().remove(dataItem);
+                            continue;
+                        }
+                    }
+                    var item = dataItem['ProductImages'];
+                    if (item && item.length > 0) {
+                        dataItem['ProductImagesUrl'] =
+                            processImage(item[0]);
+                    }else{
+                        dataItem['ProductImagesUrl'] = "resources/default.png";
+                    }
+
+                    /// start flattenLocation property
+                    flattenLocationProperties(dataItem);
+                    /// end flattenLocation property
+
+                }
+            },
+            error: function(e) {
+
+                if (e.xhr) {
+                    var errorText = "";
+                    try {
+                        errorText = JSON.stringify(e.xhr);
+                    } catch (jsonErr) {
+                        errorText = e.xhr.responseText || e.xhr.statusText || 'An error has occurred!';
+                    }
+                    alert(errorText);
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'ProductName': {
+                            field: 'ProductName',
+                            defaultValue: ''
+                        },
+                        'cvPrice': {
+                            field: 'cvPrice',
+                            defaultValue: ''
+                        },
+                        'ProductImages': {
+                            field: 'ProductImages',
+                            defaultValue: ''
+                        },
+                        'ProductID': {
+                            field: 'ProductID',
+                            defaultValue: ''
+                        },
+                        'QTY': {
+                            field: 'QTY',
+                            defaultValue: ''
+                        },
+                        'ProductImagesUrl': {
+                            field: 'ProductImagesUrl',
+                            defaultValue: 'http://img1.imgtn.bdimg.com/it/u=3024194923,2121226372&fm=23&gp=0.jpg'
+                        }
+                    }
+                }
+            },
+            serverFiltering: true,
+        },
+        /// start data sources
+        /// end data sources
+        productListViewModel = kendo.observable({
+            _dataSourceOptions: dataSourceOptions,
+            fixHierarchicalData: function(data) {
+                var result = {},
+                    layout = {};
+
+                $.extend(true, result, data);
+
+                (function removeNulls(obj) {
+                    var i, name,
+                        names = Object.getOwnPropertyNames(obj);
+
+                    for (i = 0; i < names.length; i++) {
+                        name = names[i];
+
+                        if (obj[name] === null) {
+                            delete obj[name];
+                        } else if ($.type(obj[name]) === 'object') {
+                            removeNulls(obj[name]);
+                        }
+                    }
+                })(result);
+
+                (function fix(source, layout) {
+                    var i, j, name, srcObj, ltObj, type,
+                        names = Object.getOwnPropertyNames(layout);
+
+                    if ($.type(source) !== 'object') {
+                        return;
+                    }
+
+                    for (i = 0; i < names.length; i++) {
+                        name = names[i];
+                        srcObj = source[name];
+                        ltObj = layout[name];
+                        type = $.type(srcObj);
+
+                        if (type === 'undefined' || type === 'null') {
+                            source[name] = ltObj;
+                        } else {
+                            if (srcObj.length > 0) {
+                                for (j = 0; j < srcObj.length; j++) {
+                                    fix(srcObj[j], ltObj[0]);
+                                }
+                            } else {
+                                fix(srcObj, ltObj);
+                            }
+                        }
+                    }
+                })(result, layout);
+
+                return result;
+            },
+            itemClick: function(e) {
+                var dataItem = e.dataItem || productListViewModel.originalItem;
+                curItem = dataItem;
+                app.mobileApp.navigate('components/productListView/details.html?uid=' + dataItem.uid+'&productId='+dataItem.productId);
+
+            },
+            detailsShow: function(e) {
+                var uid = e.view.params.uid,
+                    dataSource = productListViewModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(uid);
+                var photos = new Array();
+                for (var i = 0; i < curItem['ProductImages'].length; i++) {
+                    var photo = new Object();
+                    photo.img = processImage(curItem['ProductImages'][i]);
+                    photos.push(photo);
+                }
+                var template = kendo.template($("#SmallGalleryTemplate").html());
+                photoDS = new kendo.data.DataSource({
+                    data: photos,
+                    change: function () {
+                        $("#SmallGalleryInner").html(kendo.render(template, photos));
+                    },
+                    pageSize:photos.length
+                });
+                photoDS.read();
+                // setTimeout(function() {
+                //     if (app.currentUser.Id.length > 0) {
+                //         window.location.href = "#userinfo";
+                //     }
+                // }, 2000);
+                productListViewModel.setCurrentItemByUid(uid);
+
+                /// start detail form show
+                /// end detail form show
+            },
+            setCurrentItemByUid: function(uid) {
+                var item = uid,
+                    dataSource = productListViewModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+                var imgitem = itemModel.ProductImages;
+                if (typeof imgitem != 'undefined' && imgitem.count>0){
+                    itemModel.ProductImagesUrl = processImage(imgitem[0]);
+                }
+
+                if (!itemModel.ProductName) {
+                    itemModel.ProductName = String.fromCharCode(160);
+                }
+
+                /// start detail form initialization
+                /// end detail form initialization
+
+                var descitem = itemModel.ProductDescription;
+                if (!descitem.count>0) {
+                    productListViewModel.set("productDesc",descitem[0]);
+                }
+                if (!itemModel.ProductID) {
+                    productListViewModel.set("ProductID",itemModel.ProductID);
+                }
+                productListViewModel.set('originalItem', itemModel);
+                productListViewModel.set('currentItem',
+                    productListViewModel.fixHierarchicalData(itemModel));
+
+                return itemModel;
+            },
+            linkBind: function(linkString) {
+                var linkChunks = linkString.split('|');
+                if (linkChunks[0].length === 0) {
+                    return this.get('currentItem.' + linkChunks[1]);
+                }
+                return linkChunks[0] + this.get('currentItem.' + linkChunks[1]);
+            },
+            /// start masterDetails view model functions
+            /// end masterDetails view model functions
+            currentItem: {}
+        });
+
+    if (typeof dataProvider.sbProviderReady === 'function') {
+        dataProvider.sbProviderReady(function dl_sbProviderReady() {
+            parent.set('productListViewModel', productListViewModel);
+            var param = parent.get('productListViewModel_delayedFetch');
+            if (typeof param !== 'undefined') {
+                parent.set('productListViewModel_delayedFetch', undefined);
+                fetchFilteredData(param);
+            }
+        });
+    } else {
+        parent.set('productListViewModel', productListViewModel);
+    }
+
+    parent.set('onShow', function(e) {
+        keyword = "";
+        $("#search").val('');
+        $("#filterSelected").val('00');
+        var param = e.view.params.filter ? JSON.parse(e.view.params.filter) : null,
+            isListmenu = false,
+            backbutton = e.view.element && e.view.element.find('header [data-role="navbar"] .backButtonWrapper'),
+            dataSourceOptions = productListViewModel.get('_dataSourceOptions'),
+            dataSource;
+        if (param || isListmenu) {
+            backbutton.show();
+            backbutton.css('visibility', 'visible');
+        } else {
+            if (e.view.element.find('header [data-role="navbar"] [data-role="button"]').length) {
+                backbutton.hide();
+            } else {
+                backbutton.css('visibility', 'hidden');
+            }
+        }
+        dataSource = new kendo.data.DataSource(dataSourceOptions);
+        productListViewModel.set('dataSource', dataSource);
+        fetchFilteredData(param);
+    });
+
+})(app.productListView);
+
+// START_CUSTOM_CODE_productListViewModel
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+// END_CUSTOM_CODE_productListViewModel
