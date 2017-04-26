1ddf00ef11d16219ff85ef7a1eed16b0f7c03a77
ding <dhanf@163.com>
2017-04-26 22:31:53 +0800

    update

--- Air2U_dev/components/checkoutView/index.js
+++ Air2U_dev/components/checkoutView/index.js
@@ -58,21 +58,21 @@ app.localization.registerView('checkoutView');
                     return;
                 }
 
-				if (!parent.address || parent.address.length == 0) {
+                if (!parent.address || parent.address.length == 0) {
                     alert('Please type your address');
                     return;
                 }
 
-                var price = checkoutViewModel.getTotalPrice();
-                if (price > 0) {
-                   checkoutViewModel.makePayment(price, function (success, transaction) {
-                       if (success) {
-                           checkoutViewModel.updateInfo(transaction);
-                       }
-                   });
-                }else {
+                // var price = checkoutViewModel.getTotalPrice();
+                // if (price > 0) {
+                //    checkoutViewModel.makePayment(price, function (success, transaction) {
+                //        if (success) {
+                //            checkoutViewModel.updateInfo(transaction);
+                //        }
+                //    });
+                // }else {
                     checkoutViewModel.updateInfo();
-                }
+                // }
             },
 
             makePayment: function (price, callback) {
